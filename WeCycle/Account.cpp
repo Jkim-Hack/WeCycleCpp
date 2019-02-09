70c83cc3dcd31c5a6820e833db88687a416ffe38
Raghav01 <raghavsaini@outlook.com>
2019-02-09 00:37:10 -0500

    god please push these phone number parameters

--- WeCycle/Account.cpp
+++ WeCycle/Account.cpp
@@ -1,14 +1,16 @@
 #include "Account.h"
 
 
-Account::Account(std::string username, std::string password) {
+Account::Account(std::string username, std::string password, int phoneNum) {
 	this->username = username;
 	this->password = password;
+	this->phoneNum = phoneNum;
 }
 
-Account::Account(std::string username, std::string password, std::string emailAddress) {
+Account::Account(std::string username, std::string password, std::string emailAddress, int phoneNum) {
 	this->username = username;
 	this->password = password;
+	this->phoneNum = phoneNum;
 	this->emailAddress = emailAddress;
 }
 
@@ -16,9 +18,13 @@ std::map<std::string, std::map<std::string, std::string>> Account::toMap() {
 	std::map<std::string, std::string> emailPass;
 	emailPass[this->emailAddress] = this->password;
 
+	std::map<int, int> numPass;
+	numPass[this->phoneNum];
+	//I'm not sure about this line I'm just experimenting^^^
+
 	std::map<std::string, std::map<std::string, std::string>> result;
 	result[this->username] = emailPass;
-	
+	3
 	return result;
 }
 
--- WeCycle/Account.h
+++ WeCycle/Account.h
@@ -6,12 +6,13 @@
 class Account : public PushableObject
 {
 private:
+	int phoneNum;
 	std::string username;
 	std::string password;
 	std::string emailAddress;
 public:
-	Account(std::string username, std::string password);
-	Account(std::string username, std::string password, std::string emailAddress);
+	Account(std::string username, std::string password, int phoneNum);
+	Account(std::string username, std::string password, std::string emailAddress, int phoneNum);
 
 	std::map<std::string, std::map<std::string, std::string>> toMap();
 	virtual ~Account();
